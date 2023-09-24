9496a1437f990bb8ff1762d29a3cf723a84af74b
MAliSheikh <malisheikhg772@gmail.com>
2023-09-24 19:51:04 +0500

    tci toe class made

---
+++ Tic tac toe Ai/main.py
@@ -0,0 +1,11 @@
+import math
+import time
+from user import HumanPlayer, RandomComputerPlayer, SmartComputerPlayer
+
+
+class TicTacToe():
+    def __init__(self):
+        self.board = self.make_board()
+        self.current_winner = None
+
+ 
