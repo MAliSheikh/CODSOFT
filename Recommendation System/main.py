a09a387f41b3a99929ac1643a0bef325962000cc
MAliSheikh <malisheikhg772@gmail.com>
2023-09-24 19:10:30 +0500

    get data

--- Recommendation System/main.py
+++ Recommendation System/main.py
@@ -5,17 +5,13 @@ from sklearn.metrics.pairwise import linear_kernel
 
 
 def get_data(csv_file):
-    data = pd.read_csv(csv_file)
+    db_movies = pd.read_csv(csv_file)
+    return db_movies
 
-    list_data = []
-    for col in data.columns:
-        list_data.append(data[col])
-    
-    return list_data
 
 print(get_data('tmdb_5000_credits.csv'))
 
-db_movies = pd.read_csv('tmdb_5000_movies.csv')
+# db_movies = pd.read_csv('tmdb_5000_movies.csv')
 
-print(db_movies.head())
+# print(db_movies.head())
 
