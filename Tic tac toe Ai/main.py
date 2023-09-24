e50ab4f04bdda12f8b05dfb013e61cba7ab825c6
MAliSheikh <malisheikhg772@gmail.com>
2023-09-24 19:56:21 +0500

    code refactored error resolved

--- Tic tac toe Ai/main.py
+++ Tic tac toe Ai/main.py
@@ -3,23 +3,18 @@ import time
 from user import HumanPlayer, RandomComputerPlayer, SmartComputerPlayer
 
 
-class TicTacToe():
+class TicTacToe:
     def __init__(self):
-        self.board = self.make_board()
+        self.board = [' ' for _ in range(9)]
         self.current_winner = None
 
-    @staticmethod
-    def make_board():
-        return [' ' for _ in range(9)]
-
     def print_board(self):
-        for row in [self.board[i*3:(i+1) * 3] for i in range(3)]:
+        for row in [self.board[i * 3:(i + 1) * 3] for i in range(3)]:
             print('| ' + ' | '.join(row) + ' |')
 
     @staticmethod
     def print_board_nums():
-        # 0 | 1 | 2
-        number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
+        number_board = [[str(i) for i in range(j * 3, (j + 1) * 3)] for j in range(3)]
         for row in number_board:
             print('| ' + ' | '.join(row) + ' |')
 
@@ -32,24 +27,19 @@ class TicTacToe():
         return False
 
     def winner(self, square, letter):
-        # check the row
         row_ind = math.floor(square / 3)
-        row = self.board[row_ind*3:(row_ind+1)*3]
-        # print('row', row)
+        row = self.board[row_ind * 3:(row_ind + 1) * 3]
         if all([s == letter for s in row]):
             return True
         col_ind = square % 3
-        column = [self.board[col_ind+i*3] for i in range(3)]
-        # print('col', column)
+        column = [self.board[col_ind + i * 3] for i in range(3)]
         if all([s == letter for s in column]):
             return True
         if square % 2 == 0:
             diagonal1 = [self.board[i] for i in [0, 4, 8]]
-            # print('diag1', diagonal1)
             if all([s == letter for s in diagonal1]):
                 return True
             diagonal2 = [self.board[i] for i in [2, 4, 6]]
-            # print('diag2', diagonal2)
             if all([s == letter for s in diagonal2]):
                 return True
         return False
@@ -65,10 +55,8 @@ class TicTacToe():
 
 
 def play(game, x_player, o_player, print_game=True):
-
     if print_game:
         game.print_board_nums()
-
     letter = 'X'
     while game.empty_squares():
         if letter == 'O':
@@ -76,23 +64,18 @@ def play(game, x_player, o_player, print_game=True):
         else:
             square = x_player.get_move(game)
         if game.make_move(square, letter):
-
             if print_game:
                 print(letter + ' makes a move to square {}'.format(square))
                 game.print_board()
                 print('')
-
             if game.current_winner:
                 if print_game:
                     print(letter + ' wins!')
-                return letter  # ends the loop and exits the game
-            letter = 'O' if letter == 'X' else 'X'  # switches player
-
+                return letter
+            letter = 'O' if letter == 'X' else 'X'
         time.sleep(.8)
-
     if print_game:
         print('It\'s a tie!')
-
 
 
 if __name__ == '__main__':
