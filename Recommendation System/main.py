2655836aa0eea6177a0dc692d7cd3a64841265fb
MAliSheikh <malisheikhg772@gmail.com>
2023-09-24 19:08:08 +0500

    libraries impoted

--- Recommendation System/main.py
+++ Recommendation System/main.py
@@ -1,17 +1,19 @@
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
+from sklearn.metrics.pairwise import linear_kernel
 
 
-# def get_data(csv_file):
-#     data = pd.read_csv(csv_file)
+def get_data(csv_file):
+    data = pd.read_csv(csv_file)
 
-#     list_data = []
-#     for col in data.columns:
-#         list_data.append(data[col])
+    list_data = []
+    for col in data.columns:
+        list_data.append(data[col])
     
-#     return list_data
+    return list_data
 
-# print(get_data('tmdb_5000_credits.csv'))
+print(get_data('tmdb_5000_credits.csv'))
 
 db_movies = pd.read_csv('tmdb_5000_movies.csv')
 
